1346e499f3b7fc9c7d2c4a994258bbca192ec9d2
Doni Rubiagatra <doni@zero-one-group.com>
2022-05-07 19:27:08 +0700

    add 1-1-handling-standard-io

---
+++ apps/1-1-handling-standard-io/jest.config.ts
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: '1-1-handling-standard-io',
+  preset: '../../jest.preset.ts',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/1-1-handling-standard-io',
+};
